8bb2ae899c25eb6771c9f8c8df4dd7e34c1bcefb
ArhoM <9487531+sinf@users.noreply.github.com>
2024-03-21 21:35:53 +0200

    http.sh

--- http.sh
+++ http.sh
@@ -1,4 +1,5 @@
 #/bin/sh
 podman build -t localhost/epever .
-podman run --rm -it --network=host --name epeverHttp -v ./config:/app/config:ro localhost/epever -c /app/config/only-http.json
+#podman run --rm -it --network=host --name epeverHttp -v ./config:/app/config:ro localhost/epever -c /app/config/only-http.json
+podman run --rm -it -p 127.0.0.1:8000:8000 --name epeverHttp -v ./config:/app/config:ro localhost/epever -c /app/config/only-http.json
 
