1a0700ee7d0b4eb9f1a09d23bceb6159954e146d
ArhoM <9487531+sinf@users.noreply.github.com>
2024-03-29 21:56:49 +0200

    unit test for find_contig*_ranges

--- modbus_thing/unit_test.py
+++ modbus_thing/unit_test.py
@@ -15,5 +15,24 @@ class TestDing(TestCase):
     self.assertEqual(bitlist(4), [0,0,1])
     self.assertEqual(bitlist(0xAB), [1,1,0,1,0,1,0,1])
 
+  def test_contiguous_ranges(self):
+    from .device import find_contiguous_ranges
+
+    self.assertEqual(
+      list(find_contiguous_ranges([1,2,5,6,7], max_delta=1, singles=[])),
+      [[1,2], [5,6,7]])
+
+    self.assertEqual(
+      list(find_contiguous_ranges([1,2,5,6,7], max_delta=1, singles=[5])),
+      [[1,2], [5], [6,7]])
+
+    self.assertEqual(
+      list(find_contiguous_ranges([1,2,5,6,7], max_delta=3, singles=[])),
+      [[1,2,5,6,7]])
+
+    self.assertEqual(
+      list(find_contiguous_ranges([1,2,5,6,7], max_delta=3, singles=[2])),
+      [[1],[2],[5,6,7]])
+
 if __name__ == '__main__':
   unittestmain()
